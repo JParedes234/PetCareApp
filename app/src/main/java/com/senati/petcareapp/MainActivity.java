367e881c0936b0229abcad0d972533449777d4e4
JParedes <ja.pare234@outlook.com>
2025-11-02 22:47:56 -0500

    style(ui): ocultar barra de navegación en login y eliminar ActionBar del tema

--- app/src/main/java/com/senati/petcareapp/MainActivity.java
+++ app/src/main/java/com/senati/petcareapp/MainActivity.java
@@ -1,13 +1,13 @@
 package com.senati.petcareapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.senati.petcareapp.databinding.ActivityMainBinding;
@@ -24,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            int id = destination.getId();
+            if (
+                    id == R.id.loginFragment
+            ) {
+                navView.setVisibility(View.GONE);
+            } else {
+                navView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 }
